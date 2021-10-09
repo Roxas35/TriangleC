86f207703746830ed56aec93b66f1d4d2bf7d442
Roxas <72147801+Roxas35@users.noreply.github.com>
2021-10-09 16:46:01 +0200

    Update Triangle.c

--- Triangle.c
+++ Triangle.c
@@ -1,18 +1,32 @@
+//Exercice Hypothénuse.
+
+
+//On ajoute les bibliothèques pour ce programme et on ajoute pas CS50.
 #include <stdio.h>
 #include <math.h>
 
+
+//On écrit la fonction principale qui ne renvoie rien.
 int main(void)
 {
+
+//On ajoute les principales variables qui font nous servir (là ce sont des floats).  
     float AB;
     float BC;
-
+    
+//Le terminal va afficher ce que le printf correspond.
+//Le scanf va nous permettre extraire la valeur de AB.
     printf("Quel est la longueur de AB en cm : ");
     scanf("%f", &AB);
-
+    
+    
+//Le terminal va afficher ce que le printf correspond.
+//Le scanf va nous permettre extraire la valeur de BC.
     printf("Quel est la longueur de BC en cm : ");
     scanf("%f", &BC);
 
-    printf("Voici la longueur de hypothenuse : %f en cm", sqrt(AB * AB + BC * BC));
+//On va pouvoir afficher la longueur de l'hypothenuse en utilisant sqrt pour calculer les racines carrées de AB et BC pour trouver AC. 
+    printf("Voici la longueur de hypothenuse AC : %f en cm", sqrt(AB * AB + BC * BC));
 
 
 }
